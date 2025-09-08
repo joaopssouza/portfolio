7eebb9f28b6ffa2d7a5342f789ac8b3336d8d963
Joao Paulo <joaop0737@gmail.com>
2025-09-07 23:03:34 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import ProjectDetail from './pages/ProjectDetail.jsx';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projeto/:id" element={<ProjectDetail />} />
       </Routes>
+      <SpeedInsights />
     </Router>
   );
 }
