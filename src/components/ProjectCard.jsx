02c0022f32e30ebe01291c9187278e93e46c68b6
Joao Paulo <joaop0737@gmail.com>
2025-09-25 01:45:53 -0300

    fix

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,7 @@
 // src/components/ProjectCard.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ThemeToggle from '../components/ThemeToggle.jsx';
 
 
 const ProjectCard = ({ project, isLCP }) => {
